ca7066e45761446383dd86f8ae8670514b8cd963
Lauan Guermandi <lauanguermandi@gmail.com>
2020-10-03 20:15:38 -0300

    chore: configuração do lint-staged + husky + prettier

--- src/app/data/repositories/student-repository/test/student-mock.repository.ts
+++ src/app/data/repositories/student-repository/test/student-mock.repository.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import faker from 'faker';
 
 import { StudentRepository } from './../../../../core/repositories/student.repository';
 import { StudentMockMapper } from './student-mock.mapper'
@@ -15,27 +14,16 @@ import { StudentMockEntity } from './student-mock.entity';
 export class StudentMockRepository extends StudentRepository {
   private mapper = new StudentMockMapper();
 
-  // Criando um aluno com faker 
-  private createStudent = (id: number): StudentMockEntity => {
-    return {
-      id: id,
-      name: faker.name.findName(),
-      grade: faker.random.number({ min: 0, max: 10, precision: 2 })
-    }
+  students = []
+
+  constructor(studentsList: StudentMockEntity[] = []) {
+    super();
+    this.students = studentsList;
   }
-
-  // Criando varios alunos
-  private createStudents = (numStudents = 5): StudentMockEntity[] => {
-    return new Array(numStudents)
-      .fill(undefined)
-      .map(this.createStudent);
-  }
-
-  private students = this.createStudents()
-
+ 
   // Obtém todos os alunos
-  public getAllStudents(): Observable<StudentModel> {
+  public getAllStudents(): Observable<StudentMockEntity> {
     return from(this.students)
-      .pipe(map(this.mapper.mapFrom));
+      .pipe(map(this.mapper.mapTo));
   }
 }
--- src/app/data/repositories/student-repository/test/student-mock.spec.ts
+++ src/app/data/repositories/student-repository/test/student-mock.spec.ts
@@ -1,5 +1,25 @@
 import { inject, TestBed } from '@angular/core/testing';
+import faker from 'faker';
+
 import { StudentMockRepository } from './student-mock.repository';
+import { StudentMockEntity } from './student-mock.entity';
+
+ // Criando um aluno com faker 
+const createStudent = (): StudentMockEntity => {
+  return {
+    id: 0,
+    name: faker.name.findName(),
+    grade: faker.random.number({ min: 0, max: 10, precision: 2 })
+  }
+}
+
+// Criando varios alunos
+const createStudents = (numStudents = 5): StudentMockEntity[] => {
+  return new Array(numStudents)
+    .fill(undefined)
+    .map(createStudent);
+}
+
 
 describe('StudentMockMapper', () => {
   beforeEach(() => {
@@ -8,7 +28,19 @@ describe('StudentMockMapper', () => {
     });
   });
 
-  it('should be created', inject([StudentMockRepository], (service: StudentMockRepository) => {
-    expect(service).toBeTruthy();
-  }));
+  it('Testando o retorno da lista de alunos', function() {
+      var studentsList = createStudents()
+      var studentMockRepository = new StudentMockRepository(studentsList)
+      var studentsResponse = studentMockRepository.getAllStudents()
+      
+      var students = [];
+
+      studentsResponse.subscribe(
+        student => {
+          students.push(student)
+        }
+      )
+      
+      expect(studentsList).toEqual(students);
+  });
 });
